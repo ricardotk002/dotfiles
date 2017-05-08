e975b124e007074ff01fc8d5a6c80cc39d37fc84
Ricardo Diaz <ricardotk002@gmail.com>
2017-05-08 00:46:52 -0500

    Emacs: initial config files for package and evil

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,14 @@
+;; Add config subfolder
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(require 'init-packages)
+(require 'init-evil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
